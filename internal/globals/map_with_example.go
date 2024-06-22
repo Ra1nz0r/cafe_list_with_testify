ea5c3e84ec90def6e7467b2f42f46b28b869297d
Ra1nz0r <n52rus@gmail.com>
2024-06-22 19:13:33 +0300

    Complete.

--- internal/globals/map_with_example.go
+++ internal/globals/map_with_example.go
@@ -4,5 +4,5 @@ var CafeList = map[string][]string{
 	"moscow":     {"Кусочки", "Dr. Живаго", "Пушкинъ", "Tehnikum", "Sempre", "Бардак", "White Rabbit", "Nicepricecafe", "Boston", "Сахалин"},
 	"n.novgorod": {"Biblioteca", "Coffee Cake", "Джани ресторани", "Фонотека", "Хуторок Корчма", "Парк Культуры"},
 	"tula":       {"МаМа Миа", "Пряности&Радости", "Тото", "Салденс Портерхаус", "Гнездо"},
-	"chicago":    {"Free Chicago"},
+	"chicago":    {"Free Chicago", "FRESH"},
 }
